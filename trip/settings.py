bb8a827754339a12910e4a1bcbd1f3e2a8ad294b
Mucy <maggornny@gmail.com>
2026-09-07 03:55:33 +0700

    Update requirements and settings for robust Vercel build

--- trip/settings.py
+++ trip/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^kms4j6kay9c(&*o=5%@xve(lcpwt^tm_j-ss^+ezp(md84-3h'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^kms4j6kay9c(&*o=5%@xve(lcpwt^tm_j-ss^+ezp(md84-3h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
